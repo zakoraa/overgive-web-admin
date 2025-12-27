f44fc57483eafa41fed4ace962e6420234391ab2
unknown <raflisilehu20@gmail.com>
2025-12-27 23:03:33 +0700

    ref: Made adjustments to the latest news reports for campaigns can be assigned to more than one distributor.

--- src/modules/distributor_assignment/create/services/get-campaigns.ts
+++ src/modules/distributor_assignment/create/services/get-campaigns.ts
@@ -14,23 +14,23 @@ export async function getCampaigns({
     const supabase = await supabaseServer();
 
     // Ambil semua campaign_id yang sudah ada di distributor_assignments
-    const { data: assignedCampaigns, error: assignedError } = await supabase
-        .from("distributor_assignments")
-        .select("campaign_id")
-        .is("deleted_at", null);
+    // const { data: assignedCampaigns, error: assignedError } = await supabase
+    //     .from("distributor_assignments")
+    //     .select("campaign_id")
+    //     .is("deleted_at", null);
 
-    if (assignedError) throw new Error(assignedError.message);
+    // if (assignedError) throw new Error(assignedError.message);
 
-    const assignedIds = assignedCampaigns?.map((c) => c.campaign_id) || [];
+    // const assignedIds = assignedCampaigns?.map((c) => c.campaign_id) || [];
 
     // Query campaigns yang belum ditugaskan
     let query = supabase
         .from("campaigns")
         .select("id, title, category, status, ended_at", { count: "exact" })
         .eq("status", "active")
-        .gte("ended_at", today)
+        .or(`ended_at.gte.${today},ended_at.is.null`)
         .is("deleted_at", null)
-        .not("id", "in", `(${assignedIds.map((id) => `${id}`).join(",")})`)
+        // .not("id", "in", `(${assignedIds.map((id) => `${id}`).join(",")})`)
         .order("created_at", { ascending: false })
         .range(from, to);
 
